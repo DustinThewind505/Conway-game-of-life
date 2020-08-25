66a0242c1fc0b9e9f069459aa81387bddd04f135
unknown <Dustin.Guillen14@yahoo.com>
2020-08-25 12:35:52 -0600

    Basic header and basic footer

--- game-of-life/src/App.js
+++ game-of-life/src/App.js
@@ -6,9 +6,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>The Game of Life</h1>
-        <img src={logo} className="App-logo" alt="logo" />
+        <h1><img src="https://i.pinimg.com/originals/7f/28/a6/7f28a62f49535a89cb394d4672bad4b5.png"/>The Game of Life<img src="https://i.pinimg.com/originals/7f/28/a6/7f28a62f49535a89cb394d4672bad4b5.png"/></h1>
       </header>
+      <footer><img src="https://vignette.wikia.nocookie.net/contra/images/b/ba/BillRizerRuns.gif/revision/latest?cb=20171207035248"/><img src={logo} className="App-logo" alt="logo" /><img src="https://vignette.wikia.nocookie.net/contra/images/b/ba/BillRizerRuns.gif/revision/latest?cb=20171207035248"/></footer>
     </div>
   );
 }
